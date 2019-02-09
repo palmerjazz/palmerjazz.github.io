acfab3afee3104150d0c05e8745802d525fbdb26
Andrew Palmer <atp@freeshell.org>
2019-02-08 21:54:03 -0600

    Update app

---
+++ js/chunk-2d210c47.a8a2a9b2.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d210c47"],{b8fa:function(n,t,e){"use strict";e.r(t);var c=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",[e("InnerItem",{attrs:{title:"Contact"}},[n._v("Coming soon...")])],1)},o=[],a=e("73c5"),r={components:{InnerItem:a["a"]}},s=r,l=e("2877"),u=Object(l["a"])(s,c,o,!1,null,null,null);t["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d210c47.a8a2a9b2.js.map
